GNOME/network-manager-applet

commit f9023a0c242cd5f8ebcf999c57def5b7240e2299

applet: support notification filtering
This gives gnome-shell a chance to apply notification filtering on
this app.

https://bugzilla.gnome.org/show_bug.cgi?id=692523

src/applet.c
@@ -936,6 +936,7 @@ applet_do_notify (NMApplet *applet,
 
 #if HAVE_LIBNOTIFY_07
 	notify_notification_set_hint (notify, "transient", g_variant_new_boolean (TRUE));
+	notify_notification_set_hint (notify, "desktop-entry", "nm-applet");
 #else
 	notify_notification_attach_to_status_icon (notify, applet->status_icon);
 #endif

nm-applet.desktop.in
@@ -11,3 +11,4 @@ X-GNOME-Bugzilla-Bugzilla=GNOME
 X-GNOME-Bugzilla-Product=NetworkManager
 X-GNOME-Bugzilla-Component=general
 X-GNOME-Autostart-enabled=true
+X-GNOME-UsesNotifications=true

SteveDodierLazaroPhD/SandboxNMApplet

commit b7fca67b63364d266d3f7c4d5066382bc7d7afd2

Commit f9023a0c242cd5f8ebcf999c57def5b7240e2299 broke the build:
applet.c:939:2: error: passing argument 3 of
‘notify_notification_set_hint’ from incompatible pointer type [-Werror]

original bzr status:
revno: 2716
committer: Michael Biebl <biebl@debian.org>
timestamp: Wed 2013-01-30 02:17:22 +0100
message:

src/applet.c
@@ -936,7 +936,7 @@ applet_do_notify (NMApplet *applet,
 
 #if HAVE_LIBNOTIFY_07
 	notify_notification_set_hint (notify, "transient", g_variant_new_boolean (TRUE));
-	notify_notification_set_hint (notify, "desktop-entry", "nm-applet");
+	notify_notification_set_hint (notify, "desktop-entry", g_variant_new_string ("nm-applet"));
 #else
 	notify_notification_attach_to_status_icon (notify, applet->status_icon);
 #endif