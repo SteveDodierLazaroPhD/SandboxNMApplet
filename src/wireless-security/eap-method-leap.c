GNOME/network-manager-applet

commit c9476e9c07839e71aed49e820e364ad0b9f47039

wireless-security: remember username/password across pages
Several different WPA Enterprise authentication types use username and
password, but each type has its own set of widgets in the editor, so
if you switch types, you lose any username/password you typed in
before. Hack this by having the WirelessSecurity object record the
last-seen username/password, and having the EAP widgets read from
that when they're shown, and write to it when they're hidden.

src/wireless-security/eap-method-leap.c
@@ -31,6 +31,8 @@
 struct _EAPMethodLEAP {
 	EAPMethod parent;
 
+	WirelessSecurity *ws_parent;
+
 	gboolean new_connection;
 
 	GtkEntry *username_entry;
@@ -111,6 +113,32 @@ update_secrets (EAPMethod *parent, NMConnection *connection)
 	                          (HelperSecretFunc) nm_setting_802_1x_get_password);
 }
 
+static void
+widgets_realized (GtkWidget *widget, EAPMethodLEAP *method)
+{
+	if (method->ws_parent->username)
+		gtk_entry_set_text (method->username_entry, method->ws_parent->username);
+	else
+		gtk_entry_set_text (method->username_entry, "");
+
+	if (method->ws_parent->password && !method->ws_parent->always_ask)
+		gtk_entry_set_text (method->password_entry, method->ws_parent->password);
+	else
+		gtk_entry_set_text (method->password_entry, "");
+
+	gtk_toggle_button_set_active (method->show_password, method->ws_parent->show_password);
+}
+
+static void
+widgets_unrealized (GtkWidget *widget, EAPMethodLEAP *method)
+{
+	wireless_security_set_userpass (method->ws_parent,
+	                                gtk_entry_get_text (method->username_entry),
+	                                gtk_entry_get_text (method->password_entry),
+	                                (gboolean) -1,
+	                                gtk_toggle_button_get_active (method->show_password));
+}
+
 EAPMethodLEAP *
 eap_method_leap_new (WirelessSecurity *ws_parent,
                      NMConnection *connection,
@@ -135,6 +163,16 @@ eap_method_leap_new (WirelessSecurity *ws_parent,
 
 	method = (EAPMethodLEAP *) parent;
 	method->new_connection = secrets_only ? FALSE : TRUE;
+	method->ws_parent = ws_parent;
+
+	widget = GTK_WIDGET (gtk_builder_get_object (parent->builder, "eap_leap_notebook"));
+	g_assert (widget);
+	g_signal_connect (G_OBJECT (widget), "realize",
+	                  (GCallback) widgets_realized,
+	                  method);
+	g_signal_connect (G_OBJECT (widget), "unrealize",
+	                  (GCallback) widgets_unrealized,
+	                  method);
 
 	widget = GTK_WIDGET (gtk_builder_get_object (parent->builder, "eap_leap_username_entry"));
 	g_assert (widget);

src/wireless-security/eap-method-simple.c
@@ -32,6 +32,8 @@
 struct _EAPMethodSimple {
 	EAPMethod parent;
 
+	WirelessSecurity *ws_parent;
+
 	EAPMethodSimpleType type;
 	gboolean is_editor;
 	gboolean new_connection;
@@ -188,6 +190,33 @@ password_always_ask_changed (GtkToggleButton *button, EAPMethodSimple *method)
 	gtk_widget_set_sensitive (GTK_WIDGET (method->show_password), !always_ask);
 }
 
+static void
+widgets_realized (GtkWidget *widget, EAPMethodSimple *method)
+{
+	if (method->ws_parent->username)
+		gtk_entry_set_text (method->username_entry, method->ws_parent->username);
+	else
+		gtk_entry_set_text (method->username_entry, "");
+
+	if (method->ws_parent->password && !method->ws_parent->always_ask)
+		gtk_entry_set_text (method->password_entry, method->ws_parent->password);
+	else
+		gtk_entry_set_text (method->password_entry, "");
+
+	gtk_toggle_button_set_active (method->always_ask, method->ws_parent->always_ask);
+	gtk_toggle_button_set_active (method->show_password, method->ws_parent->show_password);
+}
+
+static void
+widgets_unrealized (GtkWidget *widget, EAPMethodSimple *method)
+{
+	wireless_security_set_userpass (method->ws_parent,
+	                                gtk_entry_get_text (method->username_entry),
+	                                gtk_entry_get_text (method->password_entry),
+	                                gtk_toggle_button_get_active (method->always_ask),
+	                                gtk_toggle_button_get_active (method->show_password));
+}
+
 EAPMethodSimple *
 eap_method_simple_new (WirelessSecurity *ws_parent,
                        NMConnection *connection,
@@ -219,6 +248,16 @@ eap_method_simple_new (WirelessSecurity *ws_parent,
 	method->type = type;
 	method->is_editor = is_editor;
 	method->new_connection = secrets_only ? FALSE : TRUE;
+	method->ws_parent = ws_parent;
+
+	widget = GTK_WIDGET (gtk_builder_get_object (parent->builder, "eap_simple_notebook"));
+	g_assert (widget);
+	g_signal_connect (G_OBJECT (widget), "realize",
+	                  (GCallback) widgets_realized,
+	                  method);
+	g_signal_connect (G_OBJECT (widget), "unrealize",
+	                  (GCallback) widgets_unrealized,
+	                  method);
 
 	widget = GTK_WIDGET (gtk_builder_get_object (parent->builder, "eap_simple_username_entry"));
 	g_assert (widget);

src/wireless-security/wireless-security.c
@@ -140,6 +140,12 @@ wireless_security_unref (WirelessSecurity *sec)
 		if (sec->destroy)
 			sec->destroy (sec);
 
+		g_free (sec->username);
+		if (sec->password) {
+			memset (sec->password, 0, strlen (sec->password));
+			g_free (sec->password);
+		}
+
 		if (sec->builder)
 			g_object_unref (sec->builder);
 		if (sec->ui_widget)
@@ -218,6 +224,27 @@ wireless_security_adhoc_compatible (WirelessSecurity *sec)
 	g_return_val_if_fail (sec != NULL, FALSE);
 
 	return sec->adhoc_compatible;
+}
+
+void
+wireless_security_set_userpass (WirelessSecurity *sec,
+                                const char *user,
+                                const char *password,
+                                gboolean always_ask,
+                                gboolean show_password)
+{
+	g_free (sec->username);
+	sec->username = g_strdup (user);
+
+	if (sec->password) {
+		memset (sec->password, 0, strlen (sec->password));
+		g_free (sec->password);
+	}
+	sec->password = g_strdup (password);
+
+	if (always_ask != (gboolean) -1)
+		sec->always_ask = always_ask;
+	sec->show_password = show_password;
 }
 
 void

src/wireless-security/wireless-security.h
@@ -49,6 +49,9 @@ struct _WirelessSecurity {
 	const char *default_field;
 	gboolean adhoc_compatible;
 
+	char *username, *password;
+	gboolean always_ask, show_password;
+
 	WSAddToSizeGroupFunc add_to_size_group;
 	WSFillConnectionFunc fill_connection;
 	WSUpdateSecretsFunc update_secrets;
@@ -80,6 +83,12 @@ void wireless_security_update_secrets (WirelessSecurity *sec,
 GtkWidget * wireless_security_nag_user (WirelessSecurity *sec);
 
 gboolean wireless_security_adhoc_compatible (WirelessSecurity *sec);
+
+void wireless_security_set_userpass (WirelessSecurity *sec,
+                                     const char *user,
+                                     const char *password,
+                                     gboolean always_ask,
+                                     gboolean show_password);
 
 WirelessSecurity *wireless_security_ref (WirelessSecurity *sec);
 

SteveDodierLazaroPhD/SandboxNMApplet

commit c03251d770a1b684eb8d2f981544a7b2619264fc

When you opened an existing wifi in nm-connection-editor, the username and password field were unset for WPA & WPA2 Enterprise.
(This regression was introduced by commit c9476e9c07839e71aed49e820e364ad0b9f47039)

https://bugzilla.redhat.com/show_bug.cgi?id=1000564

Signed-off-by: Thomas Haller <thaller@redhat.com>

original bzr status:
revno: 2895
committer: Thomas Haller <thaller@redhat.com>
timestamp: Wed 2013-08-28 10:49:46 +0200
message:

src/wireless-security/eap-method-leap.c
@@ -202,13 +202,6 @@ eap_method_leap_new (WirelessSecurity *ws_parent,
 	g_signal_connect (G_OBJECT (widget), "changed",
 	                  (GCallback) wireless_security_changed_cb,
 	                  ws_parent);
-	if (connection) {
-		NMSetting8021x *s_8021x;
-
-		s_8021x = nm_connection_get_setting_802_1x (connection);
-		if (s_8021x && nm_setting_802_1x_get_identity (s_8021x))
-			gtk_entry_set_text (method->username_entry, nm_setting_802_1x_get_identity (s_8021x));
-	}
 
 	if (secrets_only)
 		gtk_widget_set_sensitive (widget, FALSE);
@@ -219,10 +212,6 @@ eap_method_leap_new (WirelessSecurity *ws_parent,
 	g_signal_connect (G_OBJECT (widget), "changed",
 	                  (GCallback) wireless_security_changed_cb,
 	                  ws_parent);
-
-	/* Fill secrets, if any */
-	if (connection)
-		update_secrets (parent, connection);
 
 	widget = GTK_WIDGET (gtk_builder_get_object (parent->builder, "show_checkbutton_eapleap"));
 	g_assert (widget);

src/wireless-security/eap-method-simple.c
@@ -250,8 +250,6 @@ eap_method_simple_new (WirelessSecurity *ws_parent,
 	EAPMethod *parent;
 	EAPMethodSimple *method;
 	GtkWidget *widget;
-	gboolean not_saved = FALSE;
-	NMSetting8021x *s_8021x = NULL;
 
 	parent = eap_method_init (sizeof (EAPMethodSimple),
 	                          validate,
@@ -287,11 +285,6 @@ eap_method_simple_new (WirelessSecurity *ws_parent,
 	g_signal_connect (G_OBJECT (widget), "changed",
 	                  (GCallback) wireless_security_changed_cb,
 	                  ws_parent);
-	if (connection) {
-		s_8021x = nm_connection_get_setting_802_1x (connection);
-		if (s_8021x && nm_setting_802_1x_get_identity (s_8021x))
-			gtk_entry_set_text (method->username_entry, nm_setting_802_1x_get_identity (s_8021x));
-	}
 
 	if (secrets_only)
 		gtk_widget_set_sensitive (widget, FALSE);
@@ -323,19 +316,6 @@ eap_method_simple_new (WirelessSecurity *ws_parent,
 
 	if (secrets_only)
 		gtk_widget_hide (widget);
-
-	if (s_8021x) {
-		NMSettingSecretFlags flags = NM_SETTING_SECRET_FLAG_NONE;
-
-		nm_setting_get_secret_flags (NM_SETTING (s_8021x), NM_SETTING_802_1X_PASSWORD, &flags, NULL);
-		not_saved = (flags & NM_SETTING_SECRET_FLAG_NOT_SAVED);
-	}
-
-	gtk_toggle_button_set_active (method->always_ask, not_saved);
-
-	/* Fill secrets if there's a static (ie, not OTP) password */
-	if (connection && (not_saved == FALSE))
-		update_secrets (EAP_METHOD (method), connection);
 
 	widget = GTK_WIDGET (gtk_builder_get_object (parent->builder, "show_checkbutton_eapsimple"));
 	g_assert (widget);

src/wireless-security/wireless-security.c
@@ -238,6 +238,32 @@ wireless_security_set_userpass (WirelessSecurity *sec,
 }
 
 void
+wireless_security_set_userpass_802_1x (WirelessSecurity *sec,
+                                       NMConnection *connection)
+{
+	const char *user = NULL, *password = NULL;
+	gboolean always_ask = FALSE, show_password = FALSE;
+	NMSetting8021x  *setting;
+	NMSettingSecretFlags flags;
+
+	if (!connection)
+		goto set;
+
+	setting = nm_connection_get_setting_802_1x (connection);
+	if (!setting)
+		goto set;
+
+	user = nm_setting_802_1x_get_identity (setting);
+	password = nm_setting_802_1x_get_password (setting);
+
+	if (nm_setting_get_secret_flags (NM_SETTING (setting), NM_SETTING_802_1X_PASSWORD, &flags, NULL))
+		always_ask = flags & NM_SETTING_SECRET_FLAG_NOT_SAVED;
+
+set:
+	wireless_security_set_userpass (sec, user, password, always_ask, show_password);
+}
+
+void
 wireless_security_clear_ciphers (NMConnection *connection)
 {
 	NMSettingWirelessSecurity *s_wireless_sec;
@@ -384,6 +410,9 @@ ws_802_1x_auth_combo_init (WirelessSecurity *sec,
 		if (s_8021x && nm_setting_802_1x_get_num_eap_methods (s_8021x))
 			default_method = nm_setting_802_1x_get_eap_method (s_8021x, 0);
 	}
+
+	/* initialize WirelessSecurity userpass from connection (clear if no connection) */
+	wireless_security_set_userpass_802_1x (sec, connection);
 
 	auth_model = gtk_list_store_new (2, G_TYPE_STRING, eap_method_get_g_type ());
 

src/wireless-security/wireless-security.h
@@ -86,6 +86,8 @@ void wireless_security_set_userpass (WirelessSecurity *sec,
                                      const char *password,
                                      gboolean always_ask,
                                      gboolean show_password);
+void wireless_security_set_userpass_802_1x (WirelessSecurity *sec,
+                                            NMConnection *connection);
 
 WirelessSecurity *wireless_security_ref (WirelessSecurity *sec);
 