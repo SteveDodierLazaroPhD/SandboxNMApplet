GNOME/network-manager-applet

commit 3722a190468f5ce5898db8838611bc826d3153f1

applet: port to libnm
Based on a patch by Jiří Klimeš.

src/applet-agent.c
@@ -16,7 +16,7 @@
  * with this program; if not, write to the Free Software Foundation, Inc.,
  * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
  *
- * Copyright (C) 2011 Red Hat, Inc.
+ * Copyright 2011 - 2014 Red Hat, Inc.
  */
 
 #ifdef HAVE_CONFIG_H
@@ -25,14 +25,6 @@
 
 #include <glib/gi18n.h>
 #include <string.h>
-#include <dbus/dbus-glib.h>
-#include <nm-setting-connection.h>
-#include <nm-setting-8021x.h>
-#include <nm-setting-vpn.h>
-#include <nm-setting-wireless.h>
-#include <nm-setting-wireless-security.h>
-#include <nm-setting-wired.h>
-#include <nm-setting-pppoe.h>
 
 #define SECRET_API_SUBJECT_TO_CHANGE
 #include <libsecret/secret.h>
@@ -55,7 +47,7 @@ static const SecretSchema network_manager_secret_schema = {
 	}
 };
 
-G_DEFINE_TYPE (AppletAgent, applet_agent, NM_TYPE_SECRET_AGENT);
+G_DEFINE_TYPE (AppletAgent, applet_agent, NM_TYPE_SECRET_AGENT_OLD);
 
 #define APPLET_AGENT_GET_PRIVATE(o) (G_TYPE_INSTANCE_GET_PRIVATE ((o), APPLET_TYPE_AGENT, AppletAgentPrivate))
 
@@ -76,20 +68,18 @@ static guint signals[LAST_SIGNAL] = { 0 };
 
 /*******************************************************/
 
-#define DBUS_TYPE_G_MAP_OF_STRING (dbus_g_type_get_map ("GHashTable", G_TYPE_STRING, G_TYPE_STRING))
-
 typedef struct {
 	guint id;
 
-	NMSecretAgent *agent;
+	NMSecretAgentOld *agent;
 	NMConnection *connection;
 	char *path;
 	char *setting_name;
 	char **hints;
 	guint32 flags;
-	NMSecretAgentGetSecretsFunc get_callback;
-	NMSecretAgentSaveSecretsFunc save_callback;
-	NMSecretAgentDeleteSecretsFunc delete_callback;
+	NMSecretAgentOldGetSecretsFunc get_callback;
+	NMSecretAgentOldSaveSecretsFunc save_callback;
+	NMSecretAgentOldDeleteSecretsFunc delete_callback;
 	gpointer callback_data;
 
 	GCancellable *cancellable;
@@ -97,15 +87,15 @@ typedef struct {
 } Request;
 
 static Request *
-request_new (NMSecretAgent *agent,
+request_new (NMSecretAgentOld *agent,
              NMConnection *connection,
              const char *connection_path,
              const char *setting_name,
              const char **hints,
              guint32 flags,
-             NMSecretAgentGetSecretsFunc get_callback,
-             NMSecretAgentSaveSecretsFunc save_callback,
-             NMSecretAgentDeleteSecretsFunc delete_callback,
+             NMSecretAgentOldGetSecretsFunc get_callback,
+             NMSecretAgentOldSaveSecretsFunc save_callback,
+             NMSecretAgentOldDeleteSecretsFunc delete_callback,
              gpointer callback_data)
 {
 	static guint32 counter = 1;
@@ -149,7 +139,7 @@ request_free (Request *r)
 /*******************************************************/
 
 static void
-get_save_cb (NMSecretAgent *agent,
+get_save_cb (NMSecretAgentOld *agent,
              NMConnection *connection,
              GError *error,
              gpointer user_data)
@@ -159,7 +149,7 @@ get_save_cb (NMSecretAgent *agent,
 
 static void
 get_secrets_cb (AppletAgent *self,
-                GHashTable *secrets,
+                GVariant *secrets,
                 GError *error,
                 gpointer user_data)
 {
@@ -178,21 +168,21 @@ get_secrets_cb (AppletAgent *self,
 		 */
 		if (secrets && (r->flags != NM_SECRET_AGENT_GET_SECRETS_FLAG_NONE)) {
 			NMConnection *dupl;
-			GHashTableIter iter;
+			GVariantIter iter;
 			const char *setting_name;
 
 			/* Copy the existing connection and update its secrets */
-			dupl = nm_connection_duplicate (r->connection);
-			g_hash_table_iter_init (&iter, secrets);
-			while (g_hash_table_iter_next (&iter, (gpointer) &setting_name, NULL))
+			dupl = nm_simple_connection_new_clone (r->connection);
+			g_variant_iter_init (&iter, secrets);
+			while (g_variant_iter_next (&iter, (gpointer) &setting_name, NULL))
 				nm_connection_update_secrets (dupl, setting_name, secrets, NULL);
 
 			/* And save updated secrets to the keyring */
-			nm_secret_agent_save_secrets (NM_SECRET_AGENT (self), dupl, get_save_cb, NULL);
+			nm_secret_agent_old_save_secrets (NM_SECRET_AGENT_OLD (self), dupl, get_save_cb, NULL);
 			g_object_unref (dupl);
 		}
 
-		r->get_callback (NM_SECRET_AGENT (r->agent), r->connection, secrets, error, r->callback_data);
+		r->get_callback (NM_SECRET_AGENT_OLD (r->agent), r->connection, secrets, error, r->callback_data);
 	}
 	request_free (r);
 }
@@ -282,24 +272,6 @@ is_connection_always_ask (NMConnection *connection)
 	return FALSE;
 }
 
-static GValue *
-string_to_gvalue (const char *str)
-{
-	GValue *val;
-
-	val = g_slice_new0 (GValue);
-	g_value_init (val, G_TYPE_STRING);
-	g_value_set_string (val, str);
-	return val;
-}
-
-static void
-destroy_gvalue (gpointer data)
-{
-	g_value_unset ((GValue *) data);
-	g_slice_free (GValue, data);
-}
-
 static void
 keyring_find_secrets_cb (GObject *source,
                          GAsyncResult *result,
@@ -309,7 +281,8 @@ keyring_find_secrets_cb (GObject *source,
 	GError *error = NULL;
 	GError *search_error = NULL;
 	const char *connection_id = NULL;
-	GHashTable *secrets = NULL, *settings = NULL;
+	GVariantBuilder builder;
+	GVariant *secrets = NULL, *settings = NULL;
 	GList *list = NULL;
 	GList *iter;
 	gboolean hint_found = FALSE, ask = FALSE;
@@ -332,7 +305,7 @@ keyring_find_secrets_cb (GObject *source,
 		goto done;
 	} else if (search_error) {
 		error = g_error_new (NM_SECRET_AGENT_ERROR,
-		                     NM_SECRET_AGENT_ERROR_INTERNAL_ERROR,
+		                     NM_SECRET_AGENT_ERROR_FAILED,
 		                     "%s.%d - failed to read secrets from keyring (%s)",
 		                     __FILE__, __LINE__, search_error->message);
 		g_error_free (search_error);
@@ -350,7 +323,7 @@ keyring_find_secrets_cb (GObject *source,
 		return;
 	}
 
-	secrets = g_hash_table_new_full (g_str_hash, g_str_equal, g_free, destroy_gvalue);
+	g_variant_builder_init (&builder, NM_VARIANT_TYPE_SETTING);
 
 	/* Extract the secrets from the list of matching keyring items */
 	for (iter = list; iter != NULL; iter = g_list_next (iter)) {
@@ -369,8 +342,8 @@ keyring_find_secrets_cb (GObject *source,
 				continue;
 			}
 
-			g_hash_table_insert (secrets, g_strdup (key_name),
-			                     string_to_gvalue (secret_value_get (secret, NULL)));
+			g_variant_builder_add (&builder, "{sv}", key_name,
+			                       g_variant_new_string (secret_value_get (secret, NULL)));
 
 			/* See if this property matches a given hint */
 			if (r->hints && r->hints[0]) {
@@ -383,6 +356,7 @@ keyring_find_secrets_cb (GObject *source,
 			break;
 		}
 	}
+	secrets = g_variant_builder_end (&builder);
 
 	/* If there were hints, and none of the hints were returned by the keyring,
 	 * get some new secrets.
@@ -401,47 +375,46 @@ keyring_find_secrets_cb (GObject *source,
 	/* Returned secrets are a{sa{sv}}; this is the outer a{s...} hash that
 	 * will contain all the individual settings hashes.
 	 */
-	settings = g_hash_table_new_full (g_str_hash, g_str_equal, g_free, (GDestroyNotify) g_hash_table_destroy);
-	g_hash_table_insert (settings, g_strdup (r->setting_name), secrets);
+	g_variant_builder_init (&builder, NM_VARIANT_TYPE_CONNECTION);
+	g_variant_builder_add (&builder, "{sa{sv}}", r->setting_name, secrets);
+	settings = g_variant_builder_end (&builder);
 
 done:
 	g_list_free_full (list, g_object_unref);
 	if (ask) {
-		GHashTableIter hash_iter;
+		GVariantIter dict_iter;
 		const char *setting_name;
-		GHashTable *setting_hash;
+		GVariant *setting_dict;
 
 		/* Stuff all the found secrets into the connection for the UI to use */
-		g_hash_table_iter_init (&hash_iter, settings);
-		while (g_hash_table_iter_next (&hash_iter,
-		                               (gpointer *) &setting_name,
-		                               (gpointer *) &setting_hash)) {
+		g_variant_iter_init (&dict_iter, settings);
+		while (g_variant_iter_next (&dict_iter, "{sa{sv}}", &setting_name, &setting_dict)) {
 			nm_connection_update_secrets (r->connection,
-				                          setting_name,
-				                          setting_hash,
-				                          NULL);
+			                              setting_name,
+			                              setting_dict,
+			                              NULL);
 		}
 
 		ask_for_secrets (r);
 	} else {
 		/* Otherwise send the secrets back to NetworkManager */
-		r->get_callback (NM_SECRET_AGENT (r->agent), r->connection, error ? NULL : settings, error, r->callback_data);
+		r->get_callback (NM_SECRET_AGENT_OLD (r->agent), r->connection, error ? NULL : settings, error, r->callback_data);
 		request_free (r);
 	}
 
 	if (settings)
-		g_hash_table_destroy (settings);
+		g_variant_unref (settings);
 	g_clear_error (&error);
 }
 
 static void
-get_secrets (NMSecretAgent *agent,
+get_secrets (NMSecretAgentOld *agent,
              NMConnection *connection,
              const char *connection_path,
              const char *setting_name,
              const char **hints,
              guint32 flags,
-             NMSecretAgentGetSecretsFunc callback,
+             NMSecretAgentOldGetSecretsFunc callback,
              gpointer callback_data)
 {
 	AppletAgentPrivate *priv = APPLET_AGENT_GET_PRIVATE (agent);
@@ -518,7 +491,7 @@ get_secrets (NMSecretAgent *agent,
 /*******************************************************/
 
 static void
-cancel_get_secrets (NMSecretAgent *agent,
+cancel_get_secrets (NMSecretAgentOld *agent,
                     const char *connection_path,
                     const char *setting_name)
 {
@@ -543,7 +516,7 @@ cancel_get_secrets (NMSecretAgent *agent,
 			/* cancel outstanding keyring operations */
 			g_cancellable_cancel (r->cancellable);
 
-			r->get_callback (NM_SECRET_AGENT (r->agent), r->connection, NULL, error, r->callback_data);
+			r->get_callback (NM_SECRET_AGENT_OLD (r->agent), r->connection, NULL, error, r->callback_data);
 			g_hash_table_remove (priv->requests, GUINT_TO_POINTER (r->id));
 			g_signal_emit (r->agent, signals[CANCEL_SECRETS], 0, GUINT_TO_POINTER (r->id));
 		}
@@ -562,7 +535,7 @@ save_request_try_complete (Request *r)
 	 */
 	if (r->keyring_calls == 0) {
 		if (!g_cancellable_is_cancelled (r->cancellable))
-			r->save_callback (NM_SECRET_AGENT (r->agent), r->connection, NULL, r->callback_data);
+			r->save_callback (NM_SECRET_AGENT_OLD (r->agent), r->connection, NULL, r->callback_data);
 		request_free (r);
 	}
 }
@@ -686,12 +659,10 @@ write_one_secret_to_keyring (NMSetting *setting,
 		return;
 
 	if (NM_IS_SETTING_VPN (setting) && (g_strcmp0 (key, NM_SETTING_VPN_SECRETS) == 0)) {
-		g_return_if_fail (type == DBUS_TYPE_G_MAP_OF_STRING);
+		g_return_if_fail (type == G_TYPE_HASH_TABLE);
 
 		/* Process VPN secrets specially since it's a hash of secrets, not just one */
-		nm_setting_vpn_foreach_secret (NM_SETTING_VPN (setting),
-		                               vpn_secret_iter_cb,
-		                               r);
+		nm_setting_vpn_foreach_secret (NM_SETTING_VPN (setting), vpn_secret_iter_cb, r);
 	} else {
 		g_return_if_fail (type == G_TYPE_STRING);
 		secret = g_value_get_string (value);
@@ -701,7 +672,7 @@ write_one_secret_to_keyring (NMSetting *setting,
 }
 
 static void
-save_delete_cb (NMSecretAgent *agent,
+save_delete_cb (NMSecretAgentOld *agent,
                 NMConnection *connection,
                 GError *error,
                 gpointer user_data)
@@ -719,10 +690,10 @@ save_delete_cb (NMSecretAgent *agent,
 }
 
 static void
-save_secrets (NMSecretAgent *agent,
+save_secrets (NMSecretAgentOld *agent,
               NMConnection *connection,
               const char *connection_path,
-              NMSecretAgentSaveSecretsFunc callback,
+              NMSecretAgentOldSaveSecretsFunc callback,
               gpointer callback_data)
 {
 	AppletAgentPrivate *priv = APPLET_AGENT_GET_PRIVATE (agent);
@@ -732,7 +703,7 @@ save_secrets (NMSecretAgent *agent,
 	g_hash_table_insert (priv->requests, GUINT_TO_POINTER (r->id), r);
 
 	/* First delete any existing items in the keyring */
-	nm_secret_agent_delete_secrets (agent, connection, save_delete_cb, r);
+	nm_secret_agent_old_delete_secrets (agent, connection, save_delete_cb, r);
 }
 
 /*******************************************************/
@@ -756,7 +727,7 @@ delete_find_items_cb (GObject *source,
 	secret_password_clear_finish (result, &secret_error);
 	if (secret_error != NULL) {
 		error = g_error_new (NM_SECRET_AGENT_ERROR,
-		                     NM_SECRET_AGENT_ERROR_INTERNAL_ERROR,
+		                     NM_SECRET_AGENT_ERROR_FAILED,
 		                     "The request could not be completed (%s)",
 		                     secret_error->message);
 		g_error_free (secret_error);
@@ -767,10 +738,10 @@ delete_find_items_cb (GObject *source,
 }
 
 static void
-delete_secrets (NMSecretAgent *agent,
+delete_secrets (NMSecretAgentOld *agent,
                 NMConnection *connection,
                 const char *connection_path,
-                NMSecretAgentDeleteSecretsFunc callback,
+                NMSecretAgentOldDeleteSecretsFunc callback,
                 gpointer callback_data)
 {
 	AppletAgentPrivate *priv = APPLET_AGENT_GET_PRIVATE (agent);
@@ -807,18 +778,22 @@ applet_agent_handle_vpn_only (AppletAgent *agent, gboolean vpn_only)
 AppletAgent *
 applet_agent_new (void)
 {
-	return (AppletAgent *) g_object_new (APPLET_TYPE_AGENT,
-	                                     NM_SECRET_AGENT_IDENTIFIER, "org.freedesktop.nm-applet",
-	                                     NM_SECRET_AGENT_CAPABILITIES, NM_SECRET_AGENT_CAPABILITY_VPN_HINTS,
-	                                     NULL);
+	//return g_initable_new (APPLET_TYPE_AGENT, NULL, NULL,
+	return g_object_new (APPLET_TYPE_AGENT,
+	                       NM_SECRET_AGENT_OLD_IDENTIFIER, "org.freedesktop.nm-applet",
+	                       NM_SECRET_AGENT_OLD_CAPABILITIES, NM_SECRET_AGENT_CAPABILITY_VPN_HINTS,
+	                       NULL);
 }
 
+#if 0
+FIXME
 static void
-agent_registration_result_cb (NMSecretAgent *agent, GError *error, gpointer user_data)
+agent_registration_result_cb (NMSecretAgentOld *agent, GError *error, gpointer user_data)
 {
 	if (error)
 		g_warning ("Failed to register as an agent: (%d) %s", error->code, error->message);
 }
+#endif
 
 static void
 applet_agent_init (AppletAgent *self)
@@ -826,9 +801,6 @@ applet_agent_init (AppletAgent *self)
 	AppletAgentPrivate *priv = APPLET_AGENT_GET_PRIVATE (self);
 
 	priv->requests = g_hash_table_new (g_direct_hash, g_direct_equal);
-
-	g_signal_connect (self, NM_SECRET_AGENT_REGISTRATION_RESULT,
-	                  G_CALLBACK (agent_registration_result_cb), NULL);
 }
 
 static void
@@ -857,7 +829,7 @@ static void
 applet_agent_class_init (AppletAgentClass *agent_class)
 {
 	GObjectClass *object_class = G_OBJECT_CLASS (agent_class);
-	NMSecretAgentClass *parent_class = NM_SECRET_AGENT_CLASS (agent_class);
+	NMSecretAgentOldClass *parent_class = NM_SECRET_AGENT_OLD_CLASS (agent_class);
 
 	g_type_class_add_private (agent_class, sizeof (AppletAgentPrivate));
 

src/Makefile.am
@@ -4,12 +4,12 @@ bin_PROGRAMS = nm-applet
 
 nm_applet_CPPFLAGS = \
 	$(GTK_CFLAGS) \
-	$(NMA_CFLAGS) \
+	$(LIBNM_CFLAGS) \
 	$(LIBSECRET_CFLAGS) \
 	$(NOTIFY_CFLAGS) \
 	$(MM_GLIB_CFLAGS) \
 	$(APPINDICATOR_CFLAGS) \
-	-DLIBNM_GLIB_BUILD \
+	-DLIBNM_BUILD \
 	-DNM_VERSION_MIN_REQUIRED=NM_VERSION_0_9_10 \
 	-DNM_VERSION_MAX_ALLOWED=NM_VERSION_0_9_10 \
 	-DICONDIR=\""$(datadir)/icons"\"						\
@@ -24,7 +24,7 @@ nm_applet_CPPFLAGS = \
 	$(DBUS_GLIB_CFLAGS) \
 	-I${top_srcdir}/src/utils \
 	-I${top_srcdir}/src/wireless-security \
-	-I${top_srcdir}/src/libnm-gtk
+	-I${top_srcdir}/src/libnma
 
 BUILT_SOURCES = applet-dbus-bindings.h
 
@@ -78,15 +78,15 @@ endif
 nm_applet_LDADD = \
 	-lm \
 	$(GTK_LIBS) \
-	$(NMA_LIBS) \
 	$(DBUS_GLIB_LIBS) \
+	$(LIBNM_LIBS) \
 	$(LIBSECRET_LIBS) \
 	$(NOTIFY_LIBS) \
 	$(MM_GLIB_LIBS) \
 	$(APPINDICATOR_LIBS) \
-	${top_builddir}/src/utils/libutils-libnm-glib.la \
-	${top_builddir}/src/wireless-security/libwireless-security-libnm-glib.la \
-	${top_builddir}/src/libnm-gtk/libnm-gtk.la
+	${top_builddir}/src/utils/libutils-libnm.la \
+	${top_builddir}/src/wireless-security/libwireless-security-libnm.la \
+	${top_builddir}/src/libnma/libnma.la
 
 uidir = $(datadir)/nm-applet
 ui_DATA = gsm-unlock.ui info.ui 8021x.ui keyring.png

src/ap-menu-item.c
@@ -18,7 +18,7 @@
  * with this program; if not, write to the Free Software Foundation, Inc.,
  * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
  *
- * Copyright (C) 2005 - 2008 Red Hat, Inc.
+ * Copyright 2005 - 2014 Red Hat, Inc.
  */
 
 #ifdef HAVE_CONFIG_H
@@ -29,7 +29,8 @@
 #include <glib/gi18n.h>
 #include <string.h>
 
-#include <nm-utils.h>
+#include <NetworkManager.h>
+
 #include "ap-menu-item.h"
 #include "nm-access-point.h"
 
@@ -273,7 +274,7 @@ nm_network_menu_item_new (NMAccessPoint *ap,
 	NMNetworkMenuItem *item;
 	NMNetworkMenuItemPrivate *priv;
 	guint32 ap_flags, ap_wpa, ap_rsn;
-	const GByteArray *ssid;
+	GBytes *ssid;
 
 	item = g_object_new (NM_TYPE_NETWORK_MENU_ITEM, NULL);
 	g_assert (item);
@@ -283,8 +284,10 @@ nm_network_menu_item_new (NMAccessPoint *ap,
 	nm_network_menu_item_add_dupe (item, ap);
 
 	ssid = nm_access_point_get_ssid (ap);
-	if (ssid)
-		priv->ssid_string = nm_utils_ssid_to_utf8 (ssid);
+	if (ssid) {
+		priv->ssid_string = nm_utils_ssid_to_utf8 (g_bytes_get_data (ssid, NULL),
+		                                           g_bytes_get_size (ssid));
+	}
 	if (!priv->ssid_string)
 		priv->ssid_string = g_strdup ("<unknown>");
 

src/applet-agent.h
@@ -16,13 +16,14 @@
  * with this program; if not, write to the Free Software Foundation, Inc.,
  * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
  *
- * Copyright (C) 2011 Red Hat, Inc.
+ * Copyright 2011 - 2014 Red Hat, Inc.
  */
 
 #ifndef _APPLET_AGENT_H_
 #define _APPLET_AGENT_H_
 
-#include <nm-secret-agent.h>
+#include <NetworkManager.h>
+#include <nm-secret-agent-old.h>
 
 #define APPLET_TYPE_AGENT            (applet_agent_get_type ())
 #define APPLET_AGENT(obj)            (G_TYPE_CHECK_INSTANCE_CAST ((obj), APPLET_TYPE_AGENT, AppletAgent))
@@ -35,16 +36,16 @@
 #define APPLET_AGENT_CANCEL_SECRETS "cancel-secrets"
 
 typedef struct {
-	NMSecretAgent parent;
+	NMSecretAgentOld parent;
 } AppletAgent;
 
 typedef void (*AppletAgentSecretsCallback) (AppletAgent *self,
-                                            GHashTable *secrets,
+                                            GVariant *secrets,
                                             GError *error,
                                             gpointer user_data);
 
 typedef struct {
-	NMSecretAgentClass parent_class;
+	NMSecretAgentOldClass parent_class;
 
 	void (*get_secrets)        (AppletAgent *self,
 	                            void *request_id,

src/applet-device-bond.c
@@ -17,8 +17,8 @@
  * with this program; if not, write to the Free Software Foundation, Inc.,
  * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
  *
- * (C) Copyright 2008 Red Hat, Inc.
- * (C) Copyright 2008 Novell, Inc.
+ * Copyright 2008 - 2014 Red Hat, Inc.
+ * Copyright 2008 Novell, Inc.
  */
 
 #ifdef HAVE_CONFIG_H
@@ -28,22 +28,14 @@
 #include <glib/gi18n.h>
 #include <gtk/gtk.h>
 
-#include <nm-device.h>
-#include <nm-setting-connection.h>
-#include <nm-setting-wired.h>
-#include <nm-setting-bond.h>
-#include <nm-device-bond.h>
-#include <nm-utils.h>
-
 #include "applet.h"
 #include "applet-device-bond.h"
 #include "utils.h"
-#include "nm-ui-utils.h"
 
 static void
 bond_add_menu_item (NMDevice *device,
                     gboolean multiple_devices,
-                    GSList *connections,
+                    const GPtrArray *connections,
                     NMConnection *active,
                     GtkWidget *menu,
                     NMApplet *applet)
@@ -51,7 +43,7 @@ bond_add_menu_item (NMDevice *device,
 	char *text;
 	GtkWidget *item;
 
-	text = nma_utils_get_connection_device_name (connections->data);
+	text = nm_connection_get_virtual_device_description (connections->pdata[0]);
 	item = applet_menu_item_create_device_item_helper (device, applet, text);
 	g_free (text);
 
@@ -59,8 +51,7 @@ bond_add_menu_item (NMDevice *device,
 	gtk_menu_shell_append (GTK_MENU_SHELL (menu), item);
 	gtk_widget_show (item);
 
-	if (g_slist_length (connections))
-		applet_add_connection_items (device, connections, TRUE, active, NMA_ADD_ACTIVE, menu, applet);
+	applet_add_connection_items (device, connections, TRUE, active, NMA_ADD_ACTIVE, menu, applet);
 
 	/* Notify user of unmanaged or unavailable device */
 	if (device) {
@@ -72,11 +63,10 @@ bond_add_menu_item (NMDevice *device,
 	}
 
 	if (!device || !nma_menu_device_check_unusable (device)) {
-		if ((!active && g_slist_length (connections)) || (active && g_slist_length (connections) > 1))
+		if (!active || connections->len > 1)
 			applet_menu_item_add_complex_separator_helper (menu, applet, _("Available"));
 
-		if (g_slist_length (connections))
-			applet_add_connection_items (device, connections, TRUE, active, NMA_ADD_INACTIVE, menu, applet);
+		applet_add_connection_items (device, connections, TRUE, active, NMA_ADD_INACTIVE, menu, applet);
 	}
 }
 

src/applet-device-bridge.c
@@ -17,8 +17,8 @@
  * with this program; if not, write to the Free Software Foundation, Inc.,
  * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
  *
- * (C) Copyright 2008 Red Hat, Inc.
- * (C) Copyright 2008 Novell, Inc.
+ * Copyright 2008 - 2014 Red Hat, Inc.
+ * Copyright 2008 Novell, Inc.
  */
 
 #ifdef HAVE_CONFIG_H
@@ -28,22 +28,14 @@
 #include <glib/gi18n.h>
 #include <gtk/gtk.h>
 
-#include <nm-device.h>
-#include <nm-setting-connection.h>
-#include <nm-setting-wired.h>
-#include <nm-setting-bridge.h>
-#include <nm-device-bridge.h>
-#include <nm-utils.h>
-
 #include "applet.h"
 #include "applet-device-bridge.h"
 #include "utils.h"
-#include "nm-ui-utils.h"
 
 static void
 bridge_add_menu_item (NMDevice *device,
                       gboolean multiple_devices,
-                      GSList *connections,
+                      const GPtrArray *connections,
                       NMConnection *active,
                       GtkWidget *menu,
                       NMApplet *applet)
@@ -51,7 +43,7 @@ bridge_add_menu_item (NMDevice *device,
 	char *text;
 	GtkWidget *item;
 
-	text = nma_utils_get_connection_device_name (connections->data);
+	text = nm_connection_get_virtual_device_description (connections->pdata[0]);
 	item = applet_menu_item_create_device_item_helper (device, applet, text);
 	g_free (text);
 
@@ -59,8 +51,7 @@ bridge_add_menu_item (NMDevice *device,
 	gtk_menu_shell_append (GTK_MENU_SHELL (menu), item);
 	gtk_widget_show (item);
 
-	if (g_slist_length (connections))
-		applet_add_connection_items (device, connections, TRUE, active, NMA_ADD_ACTIVE, menu, applet);
+	applet_add_connection_items (device, connections, TRUE, active, NMA_ADD_ACTIVE, menu, applet);
 
 	/* Notify user of unmanaged or unavailable device */
 	if (device) {
@@ -72,11 +63,10 @@ bridge_add_menu_item (NMDevice *device,
 	}
 
 	if (!device || !nma_menu_device_check_unusable (device)) {
-		if ((!active && g_slist_length (connections)) || (active && g_slist_length (connections) > 1))
+		if (!active || connections->len > 1)
 			applet_menu_item_add_complex_separator_helper (menu, applet, _("Available"));
 
-		if (g_slist_length (connections))
-			applet_add_connection_items (device, connections, TRUE, active, NMA_ADD_INACTIVE, menu, applet);
+		applet_add_connection_items (device, connections, TRUE, active, NMA_ADD_INACTIVE, menu, applet);
 	}
 }
 

src/applet-device-broadband.c
@@ -27,15 +27,12 @@
 #include <glib/gi18n.h>
 #include <gtk/gtk.h>
 
-#include <nm-device.h>
-#include <nm-device-modem.h>
-#include <nm-utils.h>
+#include <NetworkManager.h>
 
 #include "applet.h"
 #include "applet-device-broadband.h"
 #include "applet-dialogs.h"
 #include "mobile-helpers.h"
-#include "nm-ui-utils.h"
 #include "mb-menu-item.h"
 
 typedef struct {
@@ -81,14 +78,16 @@ typedef struct {
 } ConnectNetworkInfo;
 
 static void
-add_and_activate_connection_done (NMClient *client,
-                                  NMActiveConnection *active,
-                                  const char *connection_path,
-                                  GError *error,
+add_and_activate_connection_done (GObject *client,
+                                  GAsyncResult *result,
                                   gpointer user_data)
 {
-	if (error)
+	GError *error = NULL;
+
+	if (!nm_client_add_and_activate_connection_finish (NM_CLIENT (client), result, &error)) {
 		g_warning ("Failed to add/activate connection: (%d) %s", error->code, error->message);
+		g_error_free (error);
+	}
 }
 
 static void
@@ -105,12 +104,13 @@ wizard_done (NMConnection *connection,
 		/* Ask NM to add the new connection and activate it; NM will fill in the
 		 * missing details based on the specific object and the device.
 		 */
-		nm_client_add_and_activate_connection (info->applet->nm_client,
-		                                       connection,
-		                                       info->device,
-		                                       "/",
-		                                       add_and_activate_connection_done,
-		                                       info->applet);
+		nm_client_add_and_activate_connection_async (info->applet->nm_client,
+		                                             connection,
+		                                             info->device,
+		                                             "/",
+		                                             NULL,
+		                                             add_and_activate_connection_done,
+		                                             info->applet);
 	}
 
 	g_object_unref (info->device);
@@ -318,7 +318,7 @@ unlock_dialog_new (NMDevice *device,
 	}
 
 	info->dialog = applet_mobile_pin_dialog_new (unlock_required,
-	                                             nma_utils_get_device_description (device));
+	                                             nm_device_get_description (device));
 
 	g_object_set_data (G_OBJECT (info->dialog), "unlock-code", GUINT_TO_POINTER (lock));
 	g_signal_connect (info->dialog, "response", G_CALLBACK (unlock_dialog_response), info);
@@ -464,7 +464,7 @@ get_secrets (SecretsRequest *req,
 	if (!device) {
 		g_set_error (error,
 		             NM_SECRET_AGENT_ERROR,
-		             NM_SECRET_AGENT_ERROR_INTERNAL_ERROR,
+		             NM_SECRET_AGENT_ERROR_FAILED,
 		             "%s.%d (%s): failed to find device for active connection.",
 		             __FILE__, __LINE__, __func__);
 		return FALSE;
@@ -701,7 +701,7 @@ add_connection_item (NMDevice *device,
 static void
 add_menu_item (NMDevice *device,
                gboolean multiple_devices,
-               GSList *connections,
+               const GPtrArray *connections,
                NMConnection *active,
                GtkWidget *menu,
                NMApplet *applet)
@@ -709,14 +709,14 @@ add_menu_item (NMDevice *device,
 	BroadbandDeviceInfo *info;
 	char *text;
 	GtkWidget *item;
-	GSList *iter;
+	int i;
 
 	info = g_object_get_data (G_OBJECT (device), "devinfo");
 
 	if (multiple_devices) {
 		const char *desc;
 
-		desc = nma_utils_get_device_description (device);
+		desc = nm_device_get_description (device);
 		text = g_strdup_printf (_("Mobile Broadband (%s)"), desc);
 	} else {
 		text = g_strdup (_("Mobile Broadband"));
@@ -770,12 +770,12 @@ add_menu_item (NMDevice *device,
 
 	/* Add the default / inactive connection items */
 	if (!nma_menu_device_check_unusable (device)) {
-		if ((!active && g_slist_length (connections)) || (active && g_slist_length (connections) > 1))
+		if ((!active && connections->len) || (active && connections->len > 1))
 			applet_menu_item_add_complex_separator_helper (menu, applet, _("Available"));
 
-		if (g_slist_length (connections)) {
-			for (iter = connections; iter; iter = g_slist_next (iter)) {
-				NMConnection *connection = NM_CONNECTION (iter->data);
+		if (connections->len) {
+			for (i = 0; i < connections->len; i++) {
+				NMConnection *connection = NM_CONNECTION (connections->pdata[i]);
 
 				if (connection != active) {
 					item = applet_new_menu_item_helper (connection, NULL, FALSE);

SteveDodierLazaroPhD/SandboxNMApplet

commit 0cdd963d9874c23262552a27ead55d1db71e35ce

g_variant_iter_next() needs a format string.
Fixes: 3722a190468f5ce5898db8838611bc826d3153f1

original bzr status:
revno: 3226
committer: Thomas Haller <thaller@redhat.com>
timestamp: Wed 2015-06-03 14:37:25 +0200
message:

src/applet-agent.c
@@ -174,7 +174,7 @@ get_secrets_cb (AppletAgent *self,
 			/* Copy the existing connection and update its secrets */
 			dupl = nm_simple_connection_new_clone (r->connection);
 			g_variant_iter_init (&iter, secrets);
-			while (g_variant_iter_next (&iter, (gpointer) &setting_name, NULL))
+			while (g_variant_iter_next (&iter, "{&s@a{sv}}", (gpointer) &setting_name, NULL))
 				nm_connection_update_secrets (dupl, setting_name, secrets, NULL);
 
 			/* And save updated secrets to the keyring */